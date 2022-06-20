d2fdec3029b74555acad97f19f62287e4cbf107b
Matthew <ikirik59@gmail.com>
2022-06-20 14:44:17 +0300

    Add basic repositories interfaces

---
+++ Repositories/IDirectoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataTransfer.Objects;
+
+namespace Repositories;
+
+public interface IDirectoryRepository
+{
+    Task<DirectoryDto> AddDirectory(DirectoryDto directory);
+    Task<DirectoryDto> GetDirectory(long directoryId);
+    Task<IEnumerable<DirectoryDto>> GetChildren(long directoryId);
+    Task<IEnumerable<DirectoryDto>> GetRoot(long userId);
+    Task<DirectoryDto> RenameDirectory(long directoryId, string newName);
+    Task<DirectoryDto> Remove(long directoryId);
+    Task SetAccessRights(long directoryId, long userId, bool? read, bool? write);
+}
