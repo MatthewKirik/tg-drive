b495e8eec3287cebe099a96d43e12dee14d830fc
Matthew <ikirik59@gmail.com>
2022-06-20 15:39:48 +0300

    Do not store user name and nickname

--- EfRepositories/Entities/UserInfoEntity.cs
+++ EfRepositories/Entities/UserInfoEntity.cs
@@ -4,7 +4,4 @@ public class UserInfoEntity
 {
     public long Id { get; set; }
     public long? StorageChannelId { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Username { get; set; }
 }
