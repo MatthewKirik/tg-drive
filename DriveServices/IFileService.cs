af10c5bb886f1e30e63de84eeefa4ed75a6f6724
Matthew <ikirik59@gmail.com>
2022-06-20 15:05:43 +0300

    Style: remove unneeded empty lines

--- DriveServices/IFileService.cs
+++ DriveServices/IFileService.cs
@@ -6,18 +6,15 @@ public interface IFileService
 {
     Task<FileDto> ChangeDescription(long id, string newDescription);
     Task<FileDto> Remove(long fileId, bool hardDelete = false);
-
     Task<IEnumerable<FileDto>> GetFiles(
         long? directoryId = null,
         int? skip = null,
         int? take = null);
-
     Task<IEnumerable<FileDto>> GetFilesByName(
         string name,
         long? directoryId = null,
         int? skip = null,
         int? take = null);
-
     Task<IEnumerable<FileDto>> GetFilesByDescription(
         string description,
         long? directoryId = null,
--- DriveServices/ITgFileService.cs
+++ DriveServices/ITgFileService.cs
@@ -6,20 +6,17 @@ public interface ITgFileService
 {
     Task<FileDto> AddFile(FileDto file);
     Task<FileDto> GetFile(long fileId, long destinationChatId);
-
     Task<IEnumerable<FileDto>> SendFiles(
         long destinationChatId,
         long? directoryId = null,
         int? skip = null,
         int? take = null);
-
     Task<IEnumerable<FileDto>> SendFilesByName(
         string name,
         long destinationChatId,
         long? directoryId = null,
         int? skip = null,
         int? take = null);
-
     Task<IEnumerable<FileDto>> SendFilesByDescription(
         string description,
         long destinationChatId,
