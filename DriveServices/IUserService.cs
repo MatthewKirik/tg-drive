3a63d64a411227fdb924acaf3af97ae3d48f8c78
Matthew <ikirik59@gmail.com>
2022-06-20 15:39:09 +0300

    Add user info support

---
+++ DriveServices/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using DataTransfer.Objects;
+
+namespace DriveServices;
+
+public interface IUserService
+{
+    Task GetUserInfo(long userId);
+    Task SetUserInfo(UserInfoDto userInfo);
+}
---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using DataTransfer.Objects;
+
+namespace Repositories;
+
+public interface IUserRepository
+{
+    Task GetUserInfo(long userId);
+    Task SetUserInfo(UserInfoDto userInfo);
+}
