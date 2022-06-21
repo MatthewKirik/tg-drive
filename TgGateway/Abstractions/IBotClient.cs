a60483c18499555684b9cda00afe45fb6c807d44
Matthew <ikirik59@gmail.com>
2022-06-21 14:25:45 +0300

    Add interfaces for telegram gateway

--- TgGateway/Abstractions/IBotClient.cs
+++ TgGateway/Abstractions/IBotClient.cs
@@ -8,7 +8,7 @@ public interface IBotClient
     Task<long> SendMenu(long chatId, MenuData data);
     Task<long> SendText(long chatId, string text, long? replyToMsgId = null);
     Task<long> ForwardMessageUnmanaged(long fromChat, long toChat, long messageId);
-    Task<IEnumerable<long>> ForwardMessagesUnmanaged(long fromChat, long toChat, IEnumerable<long> messageIds);
+    IAsyncEnumerable<long> ForwardMessagesUnmanaged(long fromChat, long toChat, IEnumerable<long> messageIds);
     Task<long> CopyMessageUnmanaged(long fromChat, long toChat, long messageId);
-    Task<IEnumerable<long>> CopyMessagesUnmanaged(long fromChat, long toChat, IEnumerable<long> messageIds);
+    IAsyncEnumerable<long> CopyMessagesUnmanaged(long fromChat, long toChat, IEnumerable<long> messageIds);
 }
---
+++ TgGateway/Abstractions/IMessageStorage.cs
@@ -0,0 +1,11 @@
+﻿using TgGateway.Models;
+
+namespace TgGateway.Abstractions;
+
+public interface IMessageStorage
+{
+    Task<TgMessage?> GetMenuMessage(long chatId);
+    Task SaveMessage(TgMessage msg);
+    Task<IEnumerable<TgMessage>> GetMessages(IEnumerable<TgMessagePurpose> filter);
+    Task DeleteMessages(IEnumerable<long> messageIds);
+}
