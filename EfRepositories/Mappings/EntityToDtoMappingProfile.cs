8db20bad05d384e3dd2549f1ed9fc85309e2440b
Ivan Baturkin <ivanmysterys@gmail.com>
2022-06-22 12:06:26 +0300

    Add mappings from entities to dtos

---
+++ EfRepositories/Mappings/EntityToDtoMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DataTransfer.Objects;
+using EfRepositories.Entities;
+
+namespace EfRepositories.Mappings;
+
+public class EntityToDtoMappingProfile : Profile
+{
+    public EntityToDtoMappingProfile()
+    {
+        CreateMap<DirectoryEntity, DirectoryDto>().ReverseMap();
+        CreateMap<FileEntity, FileDto>().ReverseMap();
+        CreateMap<UserInfoDto, UserInfoDto>().ReverseMap();
+    }
+}
