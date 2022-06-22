b61ba5dab66e11a14d06e748a6d81b49c23db779
Ivan Baturkin <ivanmysterys@gmail.com>
2022-06-22 22:02:16 +0300

    Implement file service

---
+++ DriveServices/Implementations/FileService.cs
@@ -0,0 +1,56 @@
+﻿using DataTransfer.Objects;
+using Repositories;
+
+namespace DriveServices.Implementations;
+
+public class FileService : IFileService
+{
+    private readonly IFileRepository _fileRepository;
+
+    public FileService(IFileRepository fileRepository)
+    {
+        _fileRepository = fileRepository;
+    }
+
+    public async Task<FileDto> ChangeDescription(long fileId, string newDescription)
+    {
+        var edited = await _fileRepository.ChangeDescription(fileId, newDescription);
+        return edited;
+    }
+
+    public async Task<FileDto> Remove(long fileId)
+    {
+        var removed = await _fileRepository.Remove(fileId);
+        return removed;
+    }
+
+    public async Task<IEnumerable<FileDto>> GetFiles(
+        long? directoryId = null,
+        int? skip = null,
+        int? take = null)
+    {
+        var files = await _fileRepository.GetFiles(directoryId, skip, take);
+        return files;
+    }
+
+    public async Task<IEnumerable<FileDto>> GetFilesByName(
+        string name,
+        long? directoryId = null,
+        int? skip = null,
+        int? take = null)
+    {
+        var files = await _fileRepository.GetFilesByName(name, directoryId, skip, take);
+        return files;
+    }
+
+    public async Task<IEnumerable<FileDto>> GetFilesByDescription(
+        string description,
+        long? directoryId = null,
+        int? skip = null,
+        int? take = null)
+    {
+        var files =
+            await _fileRepository.GetFilesByName(description, directoryId, skip, take);
+        return files;
+    }
+}
