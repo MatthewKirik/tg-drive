2e4162eab6da52a7a9a4f36f0111a7612a1c935e
Matthew <ikirik59@gmail.com>
2022-06-24 12:29:20 +0300

    Get storage channel id from user info

--- DriveServices/ITgFileService.cs
+++ DriveServices/ITgFileService.cs
@@ -4,25 +4,22 @@ namespace DriveServices;
 
 public interface ITgFileService
 {
-    Task<FileDto> AddFile(FileDto file, long destinationChatId);
-    Task<long> SendFile(long fileId, long destinationChatId);
+    Task<FileDto> AddFile(FileDto file);
+    Task<long> SendFile(long fileId);
 
     Task<IEnumerable<long>> SendFiles(
-        long destinationChatId,
         long? directoryId = null,
         int? skip = null,
         int? take = null);
 
     Task<IEnumerable<long>> SendFilesByName(
         string name,
-        long destinationChatId,
         long? directoryId = null,
         int? skip = null,
         int? take = null);
 
     Task<IEnumerable<long>> SendFilesByDescription(
         string description,
-        long destinationChatId,
         long? directoryId = null,
         int? skip = null,
         int? take = null);
